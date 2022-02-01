39e646b75d1674f38c847f0a9548ffa568a0b460
unknown <depeweth000@warren.k12.in.us>
2022-02-01 12:26:32 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Rect rect1 = new Rect(2,4);
+
+        System.out.println(rect1);
+
+        rect1.setWidth(5);
+        rect1.setLength(5);
+        System.out.println(rect1);
+    }
+}
---
+++ src/Rect.java
@@ -0,0 +1,25 @@
+public class Rect {
+    private double length;
+    private double width;
+
+    public Rect(double length, double width){
+        this.length=length;
+        this.width=width;
+    }
+
+    public double getLength(){
+        return this.length;
+    }
+
+    public double getWidth(){
+        return this.width;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+}
